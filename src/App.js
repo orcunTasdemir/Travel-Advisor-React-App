8fc43db9008962b757f48364182bf74d9e5fbd96
orcunTasdemir <orcntasdemir@gmail.com>
2022-07-01 00:15:45 -0500

    map api working with restaurant card views

--- src/App.js
+++ src/App.js
@@ -1,10 +1,56 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { CssBaseline, Grid } from "@material-ui/core";
+
+import { getPlacesData } from "./api";
+import Header from "./components/Header/Header";
+import List from "./components/List/List";
+import Map from "./components/Map/Map";
+import PlaceDetails from "./components/PlaceDetails/PlaceDetails";
 
 const App = () => {
+  const [places, setPlaces] = useState([]);
+  const [coordinates, setCoordinates] = useState({});
+  const [bounds, setBounds] = useState({});
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      ({ coords: { latitude, longitude } }) => {
+        setCoordinates({ lat: latitude, lng: longitude });
+      }
+    );
+  }, []);
+
+  useEffect(() => {
+    console.log(
+      "here are the coordinates and the bounds" +
+        JSON.stringify(coordinates) +
+        JSON.stringify(bounds)
+    );
+
+    getPlacesData(bounds.sw, bounds.ne).then((data) => {
+      console.log("This is the data being passed back from the API: ");
+      console.log(data);
+      setPlaces(data);
+    });
+  }, [coordinates, bounds]);
+
   return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
+    <>
+      <CssBaseline />
+      <Header />
+      <Grid container spacing={3} style={{ width: "100%" }}>
+        <Grid item xs={12} md={4}>
+          {" "}
+          <List places={places} />
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <Map
+            setCoordinates={setCoordinates}
+            setBounds={setBounds}
+            coordinates={coordinates}
+          />
+        </Grid>
+      </Grid>
+    </>
   );
 };
 
